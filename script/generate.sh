03713fbcd0a369ad063440d231ae4b78d0735a81
Peter Bloem <git@peterbloem.nl>
2022-02-14 16:36:01 +0100

    Auto-update.

--- script/generate.sh
+++ script/generate.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-#python generate.py \
-#  --title "Course details" \
-#  --dst ../lecture00 \
-#  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/00 Course details/CourseDetails.2021.key" \
-#  --base-url "https://mlvu.github.io/lecture00" \
-#  --pdf-link "https://mlvu.github.io/lectures/Course details.2021.pdf"
-
-#python generate.py \
-#  --title "Lecture 1: Introduction" \
-#  --dst ../lecture01 \
-#  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/11 Introduction/11.Introduction.0.key" \
-#  --base-url "https://mlvu.github.io/lecture01" \
-#  --pdf-link "https://mlvu.github.io/lectures/11.Introduction.annotated.pdf"
-
-#python generate.py \
-#  --title "Lecture 2: Linear Models and Search" \
-#  --dst ../lecture02 \
-#  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/12 Linear Models 1/12.Linear1.key" \
-#  --base-url "http://mlvu.github.io/lecture02" \
-#  --pdf-link "https://mlvu.github.io/lectures/12.LinearModels1.annotated.pdf"
+python generate.py \
+  --title "Course details" \
+  --dst ../lecture00 \
+  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/00 Course details/CourseDetails.2021.key" \
+  --base-url "https://mlvu.github.io/lecture00" \
+  --pdf-link "https://mlvu.github.io/lectures/Course details.2021.pdf"
 
 python generate.py \
-  --title "Lecture 3: Model evaluation" \
-  --dst ../lecture03 \
-  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/21 Methodology 1/21.Methodology1.key" \
-  --base-url "https://mlvu.github.io/lecture03" \
-  --pdf-link "https://mlvu.github.io/lectures/21.Methodology1.annotated.pdf"
+  --title "Lecture 1: Introduction" \
+  --dst ../lecture01 \
+  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/11 Introduction/11.Introduction.0.key" \
+  --base-url "https://mlvu.github.io/lecture01" \
+  --pdf-link "https://mlvu.github.io/lectures/11.Introduction.annotated.pdf"
+
+python generate.py \
+  --title "Lecture 2: Linear Models and Search" \
+  --dst ../lecture02 \
+  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/12 Linear Models 1/12.Linear1.key" \
+  --base-url "http://mlvu.github.io/lecture02" \
+  --pdf-link "https://mlvu.github.io/lectures/12.LinearModels1.annotated.pdf"
+
+#python generate.py \
+#  --title "Lecture 3: Model evaluation" \
+#  --dst ../lecture03 \
+#  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/21 Methodology 1/21.Methodology1.key" \
+#  --base-url "https://mlvu.github.io/lecture03" \
+#  --pdf-link "https://mlvu.github.io/lectures/21.Methodology1.annotated.pdf"
 
 #python generate.py \
 #  --title "Lecture 4: Probabilistic models" \
