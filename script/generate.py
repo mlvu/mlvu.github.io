f97d1f01578b9538334c6a60c126b960c62f2af4
Peter Bloem <git@peterbloem.nl>
2023-01-28 14:11:49 +0100

    Auto-update.

--- script/generate.py
+++ script/generate.py
@@ -304,7 +304,6 @@ def generate(
                 if node.attrib['{http://developer.apple.com/namespaces/sf}style'] in sub:
                     node.tag = 'sub'
             elif node.tag == 'p':
-                print(str(node))
                 if node.attrib['{http://developer.apple.com/namespaces/sf}style'] in li:
                     classes.append('list-item')
                 elif node.attrib['{http://developer.apple.com/namespaces/sf}style'] in itpar:
