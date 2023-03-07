6155e474624ec095ce2adc7dc3d86a9509bd97c9
Peter Bloem <git@peterbloem.nl>
2023-03-07 14:56:17 +0100

    Auto-update.

--- script/generate.sh
+++ script/generate.sh
@@ -78,12 +78,12 @@
 #  --base-url "https://mlvu.github.io/lecture09" \
 #  --pdf-link "https://mlvu.github.io/lectures/51.Deep%20Learning2.annotated.pdf"
 #
-python generate.py \
-  --title "Lecture 10: Trees and ensembles" \
-  --dst ../lecture10 \
-  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/52 Tree Models/52.Trees.key" \
-  --base-url "https://mlvu.github.io/lecture10" \
-  --pdf-link "https://mlvu.github.io/lectures/52.Trees.annotated.pdf"
+#python generate.py \
+#  --title "Lecture 10: Trees and ensembles" \
+#  --dst ../lecture10 \
+#  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/52 Tree Models/52.Trees.key" \
+#  --base-url "https://mlvu.github.io/lecture10" \
+#  --pdf-link "https://mlvu.github.io/lectures/52.Trees.annotated.pdf"
 
 #python generate.py \
 #  --title "Lecture 11: Sequences" \
