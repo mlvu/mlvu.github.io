47753dabc604eb6740ec79022121d17656443f32
Peter Bloem <git@peterbloem.nl>
2023-06-21 17:18:18 +0200

    Auto-update.

--- script/generate.sh
+++ script/generate.sh
@@ -22,12 +22,12 @@
 #  --base-url "https://mlvu.github.io/introduction" \
 #  --pdf-link "https://mlvu.github.io/lectures/11.Introduction.annotated.pdf"
 
-python generate.py \
-  --title "Lecture 2: Linear Models and Search" \
-  --dst ../lecture02 \
-  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/12 Linear Models 1/12.Linear1.key" \
-  --base-url "http://mlvu.github.io/lecture02" \
-  --pdf-link "https://mlvu.github.io/lectures/12.LinearModels1.annotated.pdf"
+#python generate.py \
+#  --title "Lecture 2: Linear Models and Search" \
+#  --dst ../lecture02 \
+#  --source "/Users/peter/Dropbox/onderwijs/Machine Learning/Lectures/12 Linear Models 1/12.Linear1.key" \
+#  --base-url "http://mlvu.github.io/lecture02" \
+#  --pdf-link "https://mlvu.github.io/lectures/12.LinearModels1.annotated.pdf"
 
 #python generate.py \
 #  --title "Lecture 3: Model evaluation" \
